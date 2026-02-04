169660309fe7d3ddb4bd27c679027d0c297cf1ac
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-02-04 17:33:24 +0100

    Improved: move wizard content to its own template file.

--- src/Helper.php
+++ src/Helper.php
@@ -23,4 +23,27 @@ class Helper {
 	public static function is_setup_completed() {
 		return (bool) get_option( Settings::SETUP_COMPLETED, false );
 	}
+	
+	/**
+	 * Render an admin view template.
+	 *
+	 * @param string $view_name The name of the view file (without .phtml extension).
+	 * @param array $data Optional. Data to pass to the view.
+	 *
+	 * @return void
+	 */
+	public static function render_admin_view( $view_name, $data = [] ) {
+		$view_file = CC_PLUGIN_DIR . 'views/admin/' . $view_name . '.phtml';
+		
+		if ( ! file_exists( $view_file ) ) {
+			return;
+		}
+		
+		// Extract data array to variables for use in the template
+		if ( ! empty( $data ) ) {
+			extract( $data );
+		}
+		
+		include $view_file;
+	}
 }
